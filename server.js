52321f79ce4189aee3bce5edc8ad210486bbb08c
Gopalakrishnan <gopalkris96@gmail.com>
2022-01-26 03:31:32 +0530

    updates to commit to heroku

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 //Install express server
 const express = require('express');
 const path = require('path');
- 
+
 const app = express();
- 
-// Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
- 
-app.get('/*', function(req,res) {
- 
-res.sendFile(path.join(__dirname+'/angularapp/index.html'));
-});
- 
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/color-relator'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/color-relator/'}),
+);
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
